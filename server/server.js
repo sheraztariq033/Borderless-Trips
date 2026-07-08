593f12179ada55e8efebbb6f205de2e6600cdd0e
Boardless Admin <admin@boardlesstrips.com>
2026-07-07 19:20:18 -0700

    Update catch-all routing parameter wildcard format to prevent path-to-regexp crash

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ app.use('/api/document-templates', documentTemplateRoutes);
 // Optional: Serve frontend static files in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 } else {
